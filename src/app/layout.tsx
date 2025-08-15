a28d26f0babe7945634cdc2e88291ed4e8eb41fd
joaotuliojt <joaotuliosoares@hotmail.com>
2025-08-15 16:56:15 -0300

    chore: add prettier + lint-staged

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import { DM_Sans, Rubik } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { DM_Sans, Rubik } from 'next/font/google';
+import './globals.css';
 
 const rubik = Rubik({
-  variable: "--font-rubik",
-  subsets: ["latin"],
+  variable: '--font-rubik',
+  subsets: ['latin'],
 });
 
 const dmSans = DM_Sans({
-  variable: "--font-dm-sans",
-  subsets: ["latin"],
+  variable: '--font-dm-sans',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Labs Spotify",
-  description: "Labs Spotify",
+  title: 'Labs Spotify',
+  description: 'Labs Spotify',
 };
 
 export default function RootLayout({
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${rubik.variable} ${dmSans.variable} antialiased`}
-      >
+      <body className={`${rubik.variable} ${dmSans.variable} antialiased`}>
         {children}
       </body>
     </html>
